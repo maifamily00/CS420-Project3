8c642fb7de1097483e21da4b648206558b972882
seankim <seankim@cpp.edu>
2017-03-03 02:08:33 -0800

    small changes

--- 4line.java
+++
@@ -1,7 +0,0 @@
-public class 4line
-{
-    public static void main(String[] args)
-    {
-        System.out.println("Hello World");
-    }
-}
---
+++ FourLine.java
@@ -0,0 +1,7 @@
+public class FourLine
+{
+  public static void main(String[] args)
+  {
+    System.out.println("Hello World");
+  }
+}
