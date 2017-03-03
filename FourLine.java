3f270cf520688aa7bd87b0963870ec76aca27c7b
seankim <seankim@cpp.edu>
2017-03-03 02:42:17 -0800

    Last bit of code for tonight 2:42 AM :P

--- FourLine.java
+++ FourLine.java
@@ -7,8 +7,7 @@ public class FourLine
 
   public static void main(String[] args) // used for testing methods
   {
-    System.out.println("Hello World");
-    FourLine fl = new FourLine();
+    FourLine fl = new FourLine();// test instantiate the object to run some of the methods
     fl.setup(); // test setup
     fl.printBoard(); // test printBoard
   }
@@ -36,9 +35,45 @@ public class FourLine
     }
   }
 
-  public void checkForWinner()
+  public int checkForWinner() // checks the state of the board to determine the winner (Probably gonna rewrite this portion -Sean 2:41 AM :P)
   {
+    // check the rows for player 1
+    for(int i = 0; i < 5; i++)
+    {
+      for(int j = 0; j < 8; j++)
+      {
+        if(board[i][j] == 1 && board[i + 1][j] == 1 && board[i + 2][j] == 1 && board[i + 3][j] == 1)
+        {
+          return 5000; // possibly filler values
+        }
+      }
+    }
 
+    // check the rows for player 2
+    for(int i = 0; i < 5; i++) // ceiling is 5 since were checking just the rows for 4 consecutive 1's
+    {
+      for(int j = 0; j < 8; j++)
+      {
+        if(board[i][j] == 2 && board[i + 1][j] == 2 && board[i + 2][j] == 2 && board[i + 3][j] == 2)
+        {
+          return -5000; // possibly filler values
+        }
+      }
+    }
+
+    // check to see if to continue the game
+    for(int i = 0; i < 8; i++)
+    {
+      for(int j = 0; j < 8; j++)
+      {
+        if(board[i][j] == 0)
+        {
+          return 0;
+        }
+      }
+    }
+
+    return 0;
   }
 
 }
